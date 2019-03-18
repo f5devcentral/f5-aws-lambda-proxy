6ce76b68ac4bbbc9296c83803eabe1a9585cfc68
gregcoward <g.coward@f5.com>
2019-03-18 16:38:45 -0700

    initialize

--- README.md
+++ README.md
@@ -1,2 +1,50 @@
-# f5-aws-lambda-proxy
-f5-aws-lambda-proxy
+# F5 AWS Lambda/API Gateway Proxy iRules LX Plug-in
+
+## Introduction
+
+<b><i>**This extension is community supported.**</i></b>
+
+The f5_aws_apigw_proxy iRules LX plug-in is a BIG-IP iRules LX plugin for enables the BIG-IP to act as a many-to-one proxy for both AWS API Gateway and direct Lambda function requests.  The plug-in utilizes a data-group to perform path/URI and path/LambdaFunction matching.  This allows for a single public facing proxy endpoint to service multiple backend AWS Lambda functions and APIs.
+
+### Lambda Proxy
+To proxy a Lambda function located within the same region as the F5 BIG-IP instance, create a datagroup, (<i>'aws-apis'</i>)to create path/fxn mappings  <i>fxn1 ---> glc-hello-function</i>.  To proxy a Lambda function located in a different AWS region simply prepend the region to the function name  <i>fxn1 ---> eu-west-1/glc-hello-function</i>.   
+
+<b><i>Lambda Proxy Example1:</b></i><br>POST/apigw.f5demo.net/<b>fxn1</b>  -- proxies directly to the Lambda function with the name <b>glc-hello-function</b>
+
+<b><i>Lambda Proxy outside of region Example2:</b></i><br>POST/apigw.f5demo.net/<b>fxn2</b>  -- proxies directly to the Lambda function with the region/name <b>eu-west-2/glc-hello-function</b>
+
+
+### AWS API Proxy
+To proxy AWS APIs, use the same datagroup to create path --> URI matchings -   <i>api1 ---> jbfipbsqfa.execute-api.us-east-1.amazonaws.com/prod/LambdaPub-WCGIBYB9AHI</i><br>     
+
+<b><i>API example:</b></i><br>POST/apigw.f5demo.net/<b>api1</b>  -- proxies to -- POST/<b>jbfipbsqfa.execute-api.us-east-1.amazonaws.com/prod/LambdaPub-WCGIBYB9AHI</b>
+        
+## Requirements
+
+BIG-IP VE 13.1 or later running on EC2
+
+## Installation
+
+<b>1.</b> Download and import [.tgz](https://github.com/gregcoward/f5-aws-apigw-proxy/releases/download/1.0.0/f5_aws_apigw_proxy.tgz) file into the BIGIP, (see below). 
+<br><img src="images/impwrkspace.png"><br>
+<br><img src="images/installedwrkspace.png"><br> 
+
+<br><br><b>2.</b> Create LX plugin from imported workspace   -  <b>Note:</b> must be named 'f5_aws_apigw_proxy'
+<br><br><b>TMSH command example:</b> <i>tmsh create ilx plugin f5_aws_apigw_proxy from-workspace f5_aws_apigw_proxy</i>
+<br><img src="images/newplugin.png"><br>
+
+<br><br><b>3.</b> Create and populate the data-group  //Note: must be named 'aws-apis'
+<br><img src="images/datagroup.png">
+<br><br><b>TMSH command example:</b> <i>tmsh create ltm data-group internal aws-apis type string records add { api1 { data jbfipbsqfa.execute-api.us-east-1.amazonaws.com/default/serverlessrepo-glc-publisher-LambdaPublisher-WFCGIBYB9AHI } fxn1 { data glc-hello-fxn }}</i>
+
+<br><br><b>4.</b> Create an AWS IAM role with the name:  <b><i>f5ApiProxyRole</i></b>  <br><br>The role requires, at a minimum the '<i>AmazonAPIGatewayInvokeFullAccess</i>' and '<i>AWSLambdaFullAccess</i>' permissions, (see below).  Attach the newly created role to BIG-IP ec2 instance. Refer to this [link](https://aws.amazon.com/blogs/security/easily-replace-or-attach-an-iam-role-to-an-existing-ec2-instance-by-using-the-ec2-console/) for instructions. 
+<br><br><img width="600" height="800" src="images/perms.png"><br><br>
+For a quick video run through of the installation process, check out this [video](https://www.youtube.com/watch?v=lY-LQtkKu0o).
+
+## Modification 
+
+Once the workspace has been installed, you can use the BIG-IP workspace, (see below) to view and modify the underlying iRule -tcl  and the nodejs processor
+<br><br><img src="images/workspace.png">
+
+</body>	
+</HTML>
---
+++ index.js
@@ -0,0 +1,144 @@
+// index.js
+var f5 = require('f5-nodejs');
+var http = require('http');
+var AWS = require('aws-sdk');
+var apigClientFactory = require('aws-api-gateway-client').default;
+
+var ilx = new f5.ILXServer();
+
+ilx.addMethod('apigw_proxy_call', function(req, res){
+    var body = JSON.parse(req.params()[1]);  //'{"here": "I am"}';//
+    var apiUri = req.params()[0];  //'glc-hello-sns'; //
+    var mthd = req.params()[2];  //'POST'; //
+
+    //Capture region from URI if provided, else use default of 'us-east-1'
+    const localRegion = {
+      host: '169.254.169.254',
+      path: '/latest/dynamic/instance-identity/document',
+      method: 'GET'
+    };
+
+    callbackRegion = function (response2) {
+      const bufferRegion = [];
+      response2.on('data', (data2) => {
+            bufferRegion.push(data2.toString('utf8'));
+      });
+      response2.on('end', function () {
+          const data2 = bufferRegion.join('');
+          if (response2.statusCode >= 400) {
+              console.log('ERROR: Non 200 status code recievd when requesting instance region.  Will default to us-east-1');
+              console.log(data2);
+              return;
+          }
+          retJson2 = JSON.parse(data2);
+          console.log('here is query region:', retJson2.region);
+          var defRegion = retJson2.region;
+          var awsRegions = ["us-east-1","us-east-2","us-west-1","us-west-2","ca-central-1","eu-west-1","eu-central-1","eu-west-2","eu-west-3","ap-northeast-1","ap-northeast-2","ap-southeast-1","ap-southeast-2","ap-south-1","sa-east-1","us-gov-west-1"];
+          var i = 0;
+          var lambdaRegion = 'no';
+
+          while ( i < awsRegions.length ) {
+              if (apiUri.includes(awsRegions[i])) {
+                  defRegion = awsRegions[i];
+                  lambdaRegion = 'yes';
+              }
+              i++;
+          }
+
+          // Derive Lambda funcion Name - default to us-east-1 if no region entered
+          if (lambdaRegion == 'yes') {
+              var regIndex = apiUri.indexOf(defRegion);
+              var fxIndex = apiUri.indexOf("/", regIndex);
+              lambdaName = apiUri.substring(fxIndex + 1);
+          } else {
+              lambdaName = apiUri;
+          }
+
+          const http_opts = {
+              host: '169.254.169.254',
+              path: '/latest/meta-data/iam/security-credentials/f5ApiProxyRole',
+              method: 'GET'
+          };
+          callback = function (response) {
+              const buffer = [];
+              response.on('data', (data) => {
+                  buffer.push(data.toString('utf8'));
+              });
+              response.on('end', function () {
+                  const data = buffer.join('');
+                  if (response.statusCode >= 400) {
+                      console.log('ERROR: Non 200 status code recievd when fetching credentials.  Verify if appropriate IAM role "f5ApiProxyRole" has been attached to the BIGIP instance - https://aws.amazon.com/blogs/security/easily-replace-or-attach-an-iam-role-to-an-existing-ec2-instance-by-using-the-ec2-console/');
+                      console.log(data);
+                      return;
+                  }
+
+                  retJson = JSON.parse(data);
+                  AWS.config = new AWS.Config();
+                  var accessKeyId = retJson.AccessKeyId;
+                  var secretAccessKey = retJson.SecretAccessKey;
+                  var sessionToken = retJson.Token;
+                  AWS.config.update({ accessKeyId: accessKeyId, secretAccessKey: secretAccessKey, sessionToken: sessionToken, region: defRegion });
+
+                  // Determine whether API or lambda function call based on URI presented
+                  console.log('Calling subroutine');
+                  var foundApi = apiUri.includes('execute-api.');
+                  if(foundApi){
+                  // apicall
+                    console.log("apicaller started");
+                    var pathParams = {};
+                    // Template syntax follows url-template https://www.npmjs.com/package/url-template
+                    var pathTemplate = '';
+                    var method = mthd;
+                    var additionalParams = {};
+                    var apiUrl = 'https://' + apiUri;
+                    var apiSlice = apiUri.indexOf("/");
+
+                    // Use returned temporary creds for api call
+                    var apigClient = apigClientFactory.newClient({
+                        invokeUrl: apiUrl, // REQUIRED
+                        accessKey: accessKeyId, // REQUIRED
+                        secretKey: secretAccessKey, // REQUIRED
+                        sessionToken: sessionToken, //REQUIRED
+                        region: defRegion, // REQUIRED: The region where the API is deployed.
+                    });
+
+                    //Invoke API using apigClientFactory and role temporary creds
+                    apigClient.invokeApi(pathParams, pathTemplate, method, additionalParams, body)
+                        .then(function (result) {
+                          res.reply(JSON.stringify(result.data));
+                        }).catch(function (result) {
+                            return "failed";
+                        });
+
+                  } else {
+
+                    // Defaulting to Lambda function Call
+                    lambdaBody = JSON.stringify(body);
+                    var lambda = new AWS.Lambda();
+                    var params = {
+                        FunctionName: lambdaName,   // required
+                        Payload: lambdaBody
+                      };
+
+                      lambda.invoke(params, function (err, data) {
+                        if (err) res.reply("failed");
+                        else     res.reply(data.Payload);
+                      });
+                    }
+              });
+              response.on('error', (err) => {
+                  console.log("Error on index.js", err);
+                  res.reply("failed");
+              });
+          };
+          http.request(http_opts, callback).end();
+      });
+      response2.on('error', (err) => {
+          console.log("Error calling Region", err);
+      });
+
+    };
+    http.request(localRegion, callbackRegion).end();
+});
+
+ilx.listen();
